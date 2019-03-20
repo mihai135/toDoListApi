3133618e735965f4e4feb7bd5b45aedd88741464
mihai135 <mihai135@yahoo.com>
2019-03-20 15:43:45 +0200

    final

--- src/main/java/org/fasttrackit/web/ToDoItemServlet.java
+++ src/main/java/org/fasttrackit/web/ToDoItemServlet.java
@@ -1,6 +1,7 @@
 package org.fasttrackit.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.fasttrackit.domain.ToDoItem;
 import org.fasttrackit.service.ToDoItemService;
 import org.fasttrackit.transfer.SaveToDoItemRequest;
 
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet(urlPatterns = "/to-do-items")
 public class ToDoItemServlet extends HttpServlet {
@@ -25,6 +27,22 @@ public class ToDoItemServlet extends HttpServlet {
             toDoItemService.createToDoItem(saveToDoItemRequest);
         } catch (Exception e) {
             resp.sendError(500, "Internal error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            List<ToDoItem> toDoItems = toDoItemService.getToDoItems();
+            ObjectMapper objectMapper = new ObjectMapper();
+            String responseJson = objectMapper.writeValueAsString(toDoItems);
+
+            resp.setContentType("application/json");
+            resp.getWriter().print(responseJson);
+            resp.getWriter().flush();
+        } catch (Exception e) {
+            resp.sendError(500, "There was an error processing your request. " +
+                    e.getMessage());
         }
     }
 }
